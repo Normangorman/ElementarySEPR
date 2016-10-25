d600587d00cbc2a8cd404f31c715ab1caa3880aa
Andrew Skea <Andrew Skea>
2016-10-25 12:13:05 +0100

    Unity Pong Game. Finished in 40 minutes. Really easy to make

---
+++ prototype_games/Unity_Pong/Unity_Pong/Assets/PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+
+    public float speed = 10.0f;
+    private Rigidbody2D RigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(moveUp))
+        {
+            RigidBody.velocity = Vector2.up * speed;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            RigidBody.velocity = Vector2.up * speed * -1;
+        }
+        else if (!Input.anyKey)
+        {
+            RigidBody.velocity = Vector2.zero;
+        }
+	}
+}
