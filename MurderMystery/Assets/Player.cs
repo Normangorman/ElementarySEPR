9f2dcc0577b7607c8a349910f5f7cfc9c52feecd
Andrew Skea <Andrew Skea>
2016-11-12 12:08:14 +0000

    MurderMystery game started. Got the start of a til emap and a character. Rooms are generated with rndom size.

---
+++ MurderMystery/Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public Tile tile;
+
+    private int x_position;
+    private int y_osition;
+
+    public int xPosition
+    {
+        get { return x_position; }
+        set
+        {
+            /*
+             * Get Tile underneath
+             * set its player value to true
+             */
+        }
+    }
+
+    public int xPosition
+    {
+        get { return y_position; }
+        set
+        {
+            /*
+             * Get Tile underneath
+             * set its player value to true
+             */
+        }
+    }
+
+    public Tile GetTile()
+    {
+        return Tile;
+    }
+
+    // Raycast to get the tile underneath
+    
+}
