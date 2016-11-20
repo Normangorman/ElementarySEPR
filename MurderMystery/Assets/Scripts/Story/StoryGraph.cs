a43615d72613c56642e8e8c038164af6cc753d1d
Benjamin Gorman <8076bgorman@gmail.com>
2016-11-20 21:36:52 +0000

    Add story code and main_beng scene

---
+++ MurderMystery/Assets/Scripts/Story/StoryGraph.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+public class StoryGraph {
+	/* This class represents the graph of a story in the game.
+	 * It has states which can be unlocked by the player by performing certain actions in the game.
+	 */
+
+	private class StoryGraphState {
+		/* Represents a particular state within the story.
+		 */ 
+		public readonly string title;
+		public readonly string description;
+		public readonly List<string> requirements;
+		public bool completed = false;
+		public StoryGraphState(string title, string description, List<string> requirements)
+		{
+			this.title = title;
+			this.description = description;
+			this.requirements = requirements;
+		}
+	}
+
+	private List<StoryGraphState> states = new List<StoryGraphState>(); // perhaps an an actual graph data structure would be better
+	private StoryScript storyScript;
+
+	public StoryGraph(StoryScript storyScript, string graphFilePath)
+	{
+		/* Takes a path to a json file specifying the structure of the story.
+         * See Assets/Scripts/Story/example_story.json for an example
+		 */
+		Debug.Log("Loading story graph from: " + graphFilePath);
+		this.storyScript = storyScript;
+
+        JObject graphJSON = JObject.Parse(File.ReadAllText(graphFilePath));
+        JArray statesJSON = (JArray)graphJSON.GetValue("states");
+        foreach (var state in statesJSON.Children<JObject>())
+        {
+            List<string> stateRequirements = state.Value<List<string>>("requirements");
+            states.Add(new StoryGraphState(
+                state.Value<string>("title"),
+                state.Value<string>("description"),
+                stateRequirements));
+        }
+	}
+
+	public void CompleteState(string stateTitle)
+	{
+        /* Marks the state with the given title as being completed.
+         * Also calls the StoryScript's OnStateCompleted hook for the given state.
+         * If any new states are unlocked by this completion, the OnStateUnlocked hook is called for each one.
+         */
+		Debug.Log("Setting state as completed: " + stateTitle);
+        storyScript.OnStateCompleted(stateTitle);
+
+		StoryGraphState state = GetStateWithTitle(stateTitle);
+		state.completed = true;
+
+		// Check if completing this state unlocked any new states
+		List<StoryGraphState> childStates = new List<StoryGraphState>();
+		foreach (var otherState in states)
+		{
+			if (otherState.requirements.Count > 0)
+			{
+				if (otherState.requirements.Find(x => x == stateTitle) != "")
+					childStates.Add(otherState);
+			}
+		}
+
+		if (childStates.Count > 0)
+		{
+			Debug.LogFormat("Found {} other states with this state as a parent.", childStates.Count);
+			foreach (var childState in childStates) {
+				bool allRequirementsComplete = true;
+				foreach (string requirementTitle in childState.requirements)
+				{
+					if (!GetStateWithTitle(requirementTitle).completed)
+					{
+						allRequirementsComplete = false;
+						Debug.LogFormat("Missing requirement for state {0}: {1}", childState.title, requirementTitle); 
+						break;
+					}
+				}
+
+				if (allRequirementsComplete)
+				{
+					storyScript.OnStateUnlocked(childState.title);
+				}
+			}
+		}
+	}
+
+    public bool IsStateCompleted(string stateTitle)
+    {
+        return GetStateWithTitle(stateTitle).completed;
+    }
+
+	private StoryGraphState GetStateWithTitle(string stateTitle)
+	{
+		/* Returns the state with the given title
+		 */
+		foreach (var state in states)
+		{
+			if (state.title == stateTitle)
+			{
+				return state;
+			}
+		}
+        return null;
+	}
+}
---
+++ MurderMystery/Assets/Scripts/Story/StoryManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Remoting;
+
+public class StoryManager : MonoBehaviour {
+	/* Main class for managing the story.
+	 * Game objects that care about the story should interface with this class.
+     * Specific stories are scripted using a StoryScript, and this class keeps a reference to the current StoryScript.
+	 * When particular events happen in the game this class will call a StoryScript hook.
+	 * 
+	 * e.g. When the player unlocks a new state in the story, the OnStateUnlocked hook will be called.
+	 */
+
+	// set this to be the script for the active story
+	// for assessment 2 there will probably only be 1 available storyScript
+	private StoryScript storyScript;
+
+	void Start () {
+        // TODO: Randomly pick from one of the available story scripts. For now there is just one.
+        this.storyScript = new ExampleStoryScript();
+	}
+	
+	void Update () {
+	}
+
+	public void NotifyItemFound(string itemName)
+	{
+		/* Called after the player picks up an item.
+		 */
+		Debug.Log("Item found: " + itemName);
+		storyScript.OnItemFound(itemName);
+	}
+
+	public void NotifyNPCSpokenTo(string npcName)
+	{
+		/* Called after the player finishes speaking to an NPC.
+		 */
+		Debug.Log("NPC Spoken to: " + npcName);
+		storyScript.OnNPCSpokenTo(npcName);
+	}
+
+	public void NotifyPlayerEnterRoom(string roomName)
+	{
+		/* Called when the player enters a new room.
+		 */
+		Debug.Log("Player entered room: " + roomName);
+		storyScript.OnPlayerEnterRoom(roomName);
+	}
+
+	public void NotifyPlayerLeaveRoom(string roomName)
+	{
+		/* Called when the player leaves a room
+		 */
+		Debug.Log("Player left room: " + roomName);
+		storyScript.OnPlayerLeaveRoom(roomName);
+	}
+}
