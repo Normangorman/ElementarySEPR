be9714eea584e882ca22271cf96608be18778b5e
Benjamin Gorman <8076bgorman@gmail.com>
2016-11-24 10:07:56 +0000

    Copy files from USB stick (may not work unity)

---
+++ MurderMystery/Assets/trumpMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trumpMover : MonoBehaviour {
+    public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0);
+	}
+}
